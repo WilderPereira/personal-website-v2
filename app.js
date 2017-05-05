b9cd8ab189f56d93d71a9f8e8336b4e5e559e28f
Wilder Pereira <wilder_roberto@hotmail.com>
2017-05-05 00:46:23 -0300

    Added languages to angular

--- app.js
+++ app.js
@@ -37,4 +37,33 @@ var app = angular.module('materializeApp', ['ui.materialize'])
             }
         ];
 
+        $scope.languages = [
+            {
+                icon: 'assets/java.png',
+                description: 'Java',
+                proficiency: 80
+            },
+            {
+                icon: 'assets/python.png',
+                description: 'Python',
+                proficiency: 65
+            },
+            {
+                icon: 'assets/kotlin.png',
+                description: 'Kotlin',
+                proficiency: 65
+            },
+            {
+                icon: 'assets/js.png',
+                description: 'Javascript',
+                proficiency: 50
+            },
+            {
+                icon: 'assets/golang.png',
+                description: 'Golang',
+                proficiency: 45
+            }
+           
+        ];
+
 }]);
